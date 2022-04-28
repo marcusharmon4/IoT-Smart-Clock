8a26081c7c7168b76d1729be9c022d28efb0cded
marcusharmon4 <89416198+marcusharmon4@users.noreply.github.com>
2022-04-27 21:47:11 -0500

    Add files via upload

---
+++ lcdfile.py
@@ -0,0 +1,35 @@
+from time import sleep, strftime
+
+from subprocess import *
+
+import lcddriver
+
+ 
+
+lcd = lcddriver.lcd()
+
+
+
+def run_cmd(cmd):
+
+    p = Popen(cmd, shell = True, stdout = PIPE)
+
+    output = p.communicate()[0]
+
+    return output
+
+ 
+
+lcd.lcd_clear()
+
+ 
+
+while True:
+
+    lcd.lcd_display_string(strftime('TIME: ' '%I:%M:%S %p'), 1)
+
+    lcd.lcd_display_string(strftime('%a, %b %d %Y'), 2)
+
+ 
+
+    sleep(1)
---
+++ pythonpath.py
@@ -0,0 +1,9 @@
+import sys
+
+import os
+
+#print(sys.path)
+
+ 
+
+print(os.environ.get('PYTHONPATH', ''))
